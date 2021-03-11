35f76423b8cfe1ba28588fa1f25e6d0785386dea
CH <64872230+CHonesetDoPa@users.noreply.github.com>
2021-03-11 16:47:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'https://c-proxy.vercel.app/',
+    serverName: 'c-proxy.vercel.app/',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
