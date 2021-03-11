3e1c116f3e73ca68ad5f91fe2c17f0351dad889e
CH <64872230+CHonesetDoPa@users.noreply.github.com>
2021-03-11 17:03:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'proxy.netptop.com',
+    serverName: 'c-proxy-git-master-chonesetdopa.vercel.app/',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
